44537e2751382dcc66531563bca9fbb15c2e4d75
Jiri Janota <janotjir@fel.cvut.cz>
2022-03-05 13:31:03 +0100

    updated dataset loader for pointnet

--- networks/pointnet/dataset_car_detector.py
+++ networks/pointnet/dataset_car_detector.py
@@ -15,15 +15,6 @@ def pc_normalize(pc):
 
 
 def generate_pc(point_set, label, npoints):
-    # discard zero positions
-   
-    # id = 0
-    # while id != len(label):
-    #     if point_set[id][0] == 0.0 and point_set[id][1] == 0.0 and label[id] == 0:
-    #         point_set = np.delete(point_set, id, 0)
-    #         label = np.delete(label, id)
-    #         continue
-    #     id += 1
 
     # discard zero positions like a big boy
     mask = (point_set[:, 0] == 0) & (point_set[:, 1] == 0) & (label == 0)
@@ -33,23 +24,14 @@ def generate_pc(point_set, label, npoints):
     # add/discard positions based on number of points in the frame
     act_npoints = len(point_set)
     center = np.mean(point_set[:, :2], axis=0)
-    # print(act_npoints, npoints)
-    if act_npoints < npoints:  # randomly duplicate
-        # print("Discarding")
-        for _ in range(npoints - act_npoints):
-            act_id = random.randint(0, act_npoints - 1)
-            point_set = np.vstack((point_set, [point_set[act_id]]))
-            label = np.hstack((label, label[act_id]))
-    else:  # duplicate/discard frame
-        # resample and choose accurate quantity of point set
-        choice = np.random.choice(len(label), npoints, replace=True)
+    if act_npoints < npoints:  # randomly duplicate frames
+        choice = np.random.choice(len(label), npoints-act_npoints, replace=True)
+        point_set = np.concatenate((point_set, point_set[choice]))
+        label = np.concatenate((label, label[choice]))
+    else:  # discard frames randomly
+        choice = np.random.choice(len(label), npoints, replace=False)
         point_set = point_set[choice, :]
         label = label[choice]
-        # print("Duplicating")
-        # distances = (point_set[:, 0] - center[0]) ** 2 + (point_set[:, 1] - center[1]) ** 2
-        # indexes = np.argsort(distances)
-        # label = label[indexes]
-        # point_set = point_set[indexes]
 
     return point_set, label, center
 
@@ -62,38 +44,39 @@ class CarDetectorDataset():
         self.path = path
 
         if trn:
-            trn = "trn"
+            self.path = path + "/training"
         else:
-            trn = "val"
-        with open(trn + "_list.txt") as f:
-            self.data = f.readlines()
+            self.path = path + "/testing"
+
+        self.bags = os.listdir(self.path)
+        self.cumul_num_frames = np.zeros(len(self.bags), dtype=int)
+        self.count_frames()
+
+        print(self.bags)
+
+    def count_frames(self):
+        for i in range(len(self.bags)):
+            self.cumul_num_frames[i] = sum(self.cumul_num_frames[:i]) + len(os.listdir(self.path + "/" + self.bags[i]))
 
     def __getitem__(self, index):
-        file = self.data[index]
-        file = file.split("_")
-        idx = int(file[1])
-        file = file[0]
 
-        # with h5py.File(self.path + "/" + file, 'r') as f:
-        #     points = f["data"][idx]
-        #     labels = f["labels"][idx]
-       
-        file = self.path + "/" + file
-        points = np.copy(np.load(file + ".points.npy", mmap_mode="r")[idx])
-        labels = np.copy(np.load(file + ".labels.npy", mmap_mode="r")[idx])
+        bag_id = np.min(np.nonzero(index < self.cumul_num_frames))
+        index = index - (self.cumul_num_frames[bag_id-1] if bag_id > 0 else 0)
 
+        with np.load(self.path + "/" + self.bags[bag_id] + "/" + str(index) + ".npz", mmap_mode="r") as f:
+            points = f['pc']
+            labels = f['labels']
+
+        points = points[:, :2]
         labels[labels != -1] = 1
         labels[labels == -1] = 0
 
-        # print("Before {}".format(len(points)))
         point_set, label, center = generate_pc(points, labels, self.npoints)
-        # print("After {}".format(len(point_set)))
         point_set[:, 0:2] = point_set[:, 0:2] - center
         if self.normalize:
             point_set = pc_normalize(point_set)
-        # print("Final {}".format(len(point_set)))
 
         return point_set, label
 
     def __len__(self):
-        return len(self.data)
+        return self.cumul_num_frames[-1]
