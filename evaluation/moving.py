394e9fcb9e1cc73bce428f136d588819039cd08c
Your Namehtop <broughton_g182@hotmail.com>
2022-05-06 00:06:06 +0100

    moving eal

--- evaluation/moving.py
+++ evaluation/moving.py
@@ -15,7 +15,7 @@ confusionGraphLimit = 20.0
 graph_resolution = 250
 detectionThreshold = 1.0
 
-evalName = "eval-graphtest"
+evalName = "moving"
 datasetPaths = {"../data/results/detector-s": "annotations"}
 datasetPaths["../data/results/temporal-new-0.8-50-5-50-10"] = "extrapolated"
 
@@ -151,6 +151,14 @@ def evaluateFile(filename, method, filePart):
                     dx = gt[0] - j[0]
                     dy = gt[1] - j[1]
                     diff = ((dx**2) + (dy**2))**0.5
+                    if "scans" in method:
+                        print("===")
+                        #print(filename)
+                        print(frameCounter - 1)
+                        #print(method)
+                        print(gt)
+                        print(j)
+                        print(diff)
                     if diff < detectionThreshold:
                         found = True
                 if found == True:
@@ -184,9 +192,12 @@ def evaluateFile(filename, method, filePart):
                     diff = abs(jo-gto)
                     if diff > (math.pi/2):
                         if jo > gto:
+                            #jo -= (math.pi/2)
                             jo -= (math.pi)
                         else:
+                            #jo += (math.pi/2)
                             gto -= (math.pi)
+                        #print(gto, jo, diff, abs(jo-gto))
                         diff = abs(jo-gto)
                     else:
                         pass #code path only used for comments
@@ -302,7 +313,7 @@ def drawGraphs():
             
     makeGraph(gt_det_hit, gt_det_total, "GT To Detection", "Distance [m]", "Probability", os.path.join(graphPath, "gtdet" + ".png"))
     makeGraph(det_gt_hit, det_gt_total, "Detection To GT", "Distance [m]", "Probability", os.path.join(graphPath, "detgt" + ".png"))
-    makeGraph(rot_error, rot_total, "Rotational Error", "Rotational Error [rads]", "Probability", os.path.join(graphPath, "rot" + ".png"), invert=True)
+    makeGraph(rot_error, rot_total, "Rotational Error", "Rotation [rads]", "Probability", os.path.join(graphPath, "rot" + ".png"))
     try:
         makeGraph(filteredRecall, 1.0, "Recall Range", "Distance [m]", "Recall", os.path.join(graphPath, "recall" + ".png"))
         makeGraph(filteredPrecision, 1.0, "Precision Range", "Distance [m]", "Precision", os.path.join(graphPath, "precision" + ".png"))
@@ -314,7 +325,7 @@ def drawGraphs():
     with open(os.path.join(resultsPath, "conf.txt"), "w") as f:
         f.write(confusionText)
 
-def makeGraph(hists, total, label, xlabel, ylabel, filename, invert=False):
+def makeGraph(hists, total, label, xlabel, ylabel, filename):
 
     fig, ax = plt.subplots()
 
@@ -329,17 +340,14 @@ def makeGraph(hists, total, label, xlabel, ylabel, filename, invert=False):
         for key, value in hists[histKey].items():
             x.append(key)
             if type(total) is not float:
-                if invert:
-                    y.append(1-(value/total[histKey]))
-                else:
-                    y.append(value/total[histKey])
+                y.append(value/total[histKey])
             else:
                 y.append(value)
         
         ax.plot(x, y, label=histKey.split("/")[-1])
-    ax.set(xlabel=xlabel, ylabel=ylabel, title='', fontsize=18)
+    ax.set(xlabel=xlabel, ylabel=ylabel, title='')
     ax.grid(linestyle="--")
-    ax.legend(loc='upper right')
+    ax.legend(loc='lower right')
     plt.ylim([-0.02, 1.02])
     fig.savefig(filename, dpi=250)
     plt.close('all')
@@ -363,10 +371,9 @@ if __name__ == "__main__":
             #if files[0] is not method:
                 for filename in files[2]:
                     if filename[-7:] == ".pickle":
-                        if "-11-16-03-33" in filename:
-                            filePart = datasetPaths[method]
-                            evaluateFile(filename, method, filePart)
-                break
+                        #if "-11-16-03-33" in filename:
+                        filePart = datasetPaths[method]
+                        evaluateFile(filename, method, filePart)
     print("Generating Graphs")
     drawGraphs()
 
--- evaluation/utils.py
+++ evaluation/utils.py
@@ -140,6 +140,101 @@ def drawImgFromPoints(filename, points, otherPoints=[], otherColours=[], cars=[]
 
     cv2.imwrite(filename, img)
 
+def getInAnnotation(scan, annotations):
+
+    carPoints = []
+    nonCarPoints = []
+
+    for point in scan:
+
+        inAnnotation = False
+
+        for annotation in annotations:
+            if isInsideAnnotation(point[:2], annotation):
+                inAnnotation = True
+                break
+
+        if inAnnotation:
+            carPoints.append(point)
+        else:
+            nonCarPoints.append(point)
+
+    return carPoints, nonCarPoints
+
+def isInsideAnnotation(pos, annotation):
+
+    poly = getBoundaryPoints(annotation)
+
+    counterPos = (9999, pos[1])
+    intersects = 0
+
+    for idx in range(len(poly)):
+        pa = poly[idx]
+        pb = poly[0]
+        if idx != len(poly)-1:
+            pb = poly[idx+1]
+
+        print(pos, counterPos, pa, pb)
+        if lineIntersect(pos, counterPos, pa, pb):
+            print("int!")
+            intersects += 1
+
+    return intersects % 2
+
+def getBoundaryPoints(poly):
+
+    centreX = poly[0]#*scale) + (res//2)
+    centreY = poly[1]#*scale) + (res//2)
+    angle = poly[2] % (math.pi*2)
+    height = 4.85# * scale
+    width = 2.4# * scale
+
+    alpha = math.cos(angle) * 0.5
+    beta = math.sin(angle) * 0.5
+
+    a = [centreX - beta * height - alpha * width, centreY + alpha * height - beta * width]
+    b = [centreX + beta * height - alpha * width, centreY - alpha * height - beta * width]
+    c = [2 * centreX - a[0], 2 * centreY - a[1]]
+    d = [2 * centreX - b[0], 2 * centreY - b[1]]
+
+    return a, b, c, d
+
+def lineIntersect(a, b, c, d):
+    o1 = tripletOrientation(a, b, c)
+    o2 = tripletOrientation(a, b, d)
+    o3 = tripletOrientation(c, d, a)
+    o4 = tripletOrientation(c, d, b)
+
+    if o1 != o2 and o3 != o4:
+        return True
+
+    if ((o1 == 0) and colinear(a, c, b)):
+        return True
+    if ((o2 == 0) and colinear(a, d, b)):
+        return True
+    if ((o3 == 0) and colinear(c, a, d)):
+        return True
+    if ((o4 == 0) and colinear(c, b, d)):
+        return True
+    return False
+
+def tripletOrientation(a, b, c):
+    orr = ((b[1] - a[1]) * (c[0] - b[0])) - ((b[0] - a[0]) * (c[1] - b[1]))
+    if orr > 0:
+        return 1
+    elif orr < 0:
+        return -1
+    return 0
+
+def colinear(a, b, c):
+    if ((b[0] <= max(a[0], c[0])) and (b[0] >= min(a[0], c[0])) and (b[1] <= max(a[1], c[1])) and (b[1] >= min(a[1], c[1]))):
+        return True
+    else:
+        return False
+
+
+
+
 class Visualise(multiprocessing.Process):
     def __init__(self, datasetPath, outPath, path, filename):
         multiprocessing.Process.__init__(self)
